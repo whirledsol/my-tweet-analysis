9ceaa715f1aaeb25a7a23f2a74e2122d0b04b30b
Will R <astro491+homesite@gmail.com>
2021-01-10 23:00:44 -0500

    adding sentiment graphs

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import json
-import numpy
+import json,numpy,nltk,re
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
+from sentifish import Sentiment
 
 def start():
     '''
     run crunches
     '''
     tweets = parse('./data/tweet.js')
-    #crunch_timeseries_counts(tweets)
-    crunch_timeseries_sentiment(tweets)
+    username = 'whirledsol'
+    #crunch_timeseries_counts(tweets,username)
+    crunch_timeseries_sentiment(tweets,username)
 
 def parse(path):
     '''
@@ -23,14 +24,23 @@ def parse(path):
     tweets = json.loads(jsoncontent)
     return [list(x.values())[0] for x in tweets]
 
-def plot_timeseries_bar(xs,ys,title='Data'):
+def plot_timeseries(xs,ys,kind="plot",title='Data',yLabel='Number',cmap=None):
     _, ax = plt.subplots()
-    ax.bar(xs,ys)
+
+    color = 'k'
+    if cmap is not None:
+        color_scale = lambda y: (y - numpy.min(y)) / (numpy.max(y) - numpy.min(y))
+        color = cmap(color_scale(ys))
+    if kind == 'bar':
+        ax.bar(xs,ys,color=color)
+    else:
+        ax.plot(xs, ys, '-o', color=color)
+
     ax.set_title(title)
     ax.legend()
     ax.set_xlabel('Date')
     axFormatDate(ax)
-    ax.set_ylabel('Number')
+    ax.set_ylabel(yLabel)
     plt.show()
 
 def axFormatDate(ax):
@@ -39,38 +49,83 @@ def axFormatDate(ax):
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
     ax.tick_params(axis='x', rotation=90)
 
-def dayOfTweet(tweet,prop='created_at'):
+def tweet_getDate(tweet,prop='created_at',dateOnly=False):
     '''
     returns the date object of the tweet without time info
     '''
-    return datetime.strptime(tweett[prop],'%a %b %d %H:%M:%S +0000 %Y').date()
+    dt = datetime.strptime(tweet[prop],'%a %b %d %H:%M:%S +0000 %Y')
+    if dateOnly:
+        dt = dt.date()
+    return dt
 
-def crunch_timeseries_counts(tweets):
+def tweet_ismine(tweet):
+    '''
+    cleans out tweets that aren't mine
+    '''
+    return tweet['retweeted'] == False and not tweet['full_text'].startswith('RT ')
+
+def tweet_getText(tweet,clean=False):
+    '''
+    gets the tweet text
+    '''
+    text = tweet['full_text']
+    if clean:
+        text = re.sub(r'@(\w){1,15}','',text)
+        text = re.sub(r'https?:\/\/[^\s]*','',text)
+    return text
+
+def crunch_timeseries_counts(tweets,username):
     '''
     shows daily counts over time
     '''
     
-    dates = [dayOfTweet(t) for t in tweets]
+    dates = [tweet_getDate(t, True) for t in tweets]
     unique, counts = numpy.unique(dates, return_counts=True)
-    plot_timeseries_bar(unique,counts,'Tweets Per Day')
+    plot_timeseries(unique,counts,kind='bar',title=f"@{username}'s Tweet Count Per Day")
 
     #bonus: most popular days
-    bins = list(zip(unique, counts))
-    bins = sorted(bins, key=lambda x: x[1],reverse=True)[0:10]
-    print('Your Most Active Days',bins)
+    top = list(zip(unique, counts))
+    top = sorted(top, key=lambda x: x[1],reverse=True)[0:10]
+    print('Your Most Active Days',top)
 
-def crunch_timeseries_sentiment(tweets):
+def crunch_timeseries_sentiment(tweets,username):
     '''
     shows daily sentiment over time
     '''
-    mytweets = [t for t in tweets if t['retweeted'] == False]
-    mytweetsbyday = {}
-    for t in mytweets:
-        date = dayOfTweet(t)
-        mytweetsbyday[date] = mytweetsbyday[date] if date in mytweetsbyday else []
-        mytweetsbyday[date] = mytweetsbyday[date].append(t['full_text'])
+    mysentiments = [t for t in tweets if tweet_ismine(t)]
+    mysentiments = [[tweet_getDate(t),tweet_getText(t,True),0] for t in mysentiments]
 
-    print(mytweetsbyday.entities()[0:10])
+    for i in range(len(mysentiments)):
+        date,t,_ = mysentiments[i]
+        try:
+            polarity = Sentiment(t).analyze()
+            mysentiments[i] = [date,t,polarity]
+        except:
+            print('could not analyize',t)
+        
+    #bar chart
+    xs = [i[0] for i in mysentiments]
+    ys = [i[2] for i in mysentiments]
+    title = f"@{username}'s Tweet Sentiments"
+    cmap = plt.get_cmap("RdYlGn")
+    #plot_timeseries(xs, ys, title=title, kind="bar", yLabel='Positivity', cmap=cmap)
 
+    #monthly trend chart
+    trend = {} #month:numpy.mean(polarity)
+    for date,tweet,polarity in mysentiments:
+        month = datetime.strptime(date.strftime('%m-%y'),'%m-%y')
+        trend[month] = trend[month] + [polarity] if month in trend else [polarity]
+    trend = [[month,numpy.mean(polarities)] for month,polarities in trend.items()]
+    trend = sorted(trend, key=lambda x: x[0])
+    xs = [i[0] for i in trend]
+    ys = [i[1] for i in trend]
+    title  = f"@{username}'s Tweet Sentiment Trend"
+    plot_timeseries(xs, ys, title=title, yLabel='Positivity')
+
+
+    #bonus: most negative tweets
+    #neg = sorted(mysentiments, key=lambda x: x[2],reverse=False)[0:10]
+    #print('Your Most Negative Tweet',neg)
+    
 if __name__ == "__main__":
     start()
